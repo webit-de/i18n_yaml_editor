b92e3f8f3093f943583c8d5aada50fc286965dba
knugie <knugie@gmx.net>
2016-09-16 00:38:05 +0200

    Set default port when initializing App

--- test/unit/test_app.rb
+++ test/unit/test_app.rb
@@ -2,4 +2,13 @@ require 'test_helper'
 require 'i18n_yaml_editor/app'
 
 class TestApp < Minitest::Test
+  def test_default_port
+    app = App.new('')
+    assert_equal 5050, app.instance_variable_get(:@port)
+  end
+
+  def test_given_port
+    app = App.new('', 3333)
+    assert_equal 3333, app.instance_variable_get(:@port)
+  end
 end
