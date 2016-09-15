3e5c586707deb0b7755926046808fc0dd2b15c8e
knugie <knugie@gmx.net>
2016-09-16 00:39:39 +0200

    Parallelize tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,12 @@ require 'coveralls'
 Coveralls.wear!
 
 require 'minitest/autorun'
+require 'minitest/hell'
 require 'i18n_yaml_editor'
 
 module Minitest
   class Test
     include I18nYamlEditor
+    parallelize_me!
   end
 end
